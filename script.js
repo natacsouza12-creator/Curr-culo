57633e57684de0a3aa01b60b1fd791f6b52bed26
Calderaro <natacsouza12@gmail.com>
2026-02-02 19:47:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("meuModal");
+const btn = document.getElementById("btnSobre");
+const span = document.getElementsByClassName("fechar")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
